45c13bdc3f08650c6ca67310869b09572d1556c4
DESKTOP-B4J5P7P\Andy <wangzito@sheridancollege.ca>
2025-01-26 14:24:05 -0500

    Jeep rotation

--- Assets/Mechanical drawing scripts/Jeep rotation.cs
+++ Assets/Mechanical drawing scripts/Jeep rotation.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Jeeprotation : MonoBehaviour
 {
-     
+    float rotationSpeed = 80f; // rotation speed
+    float Angle = 0f; // set angle degres
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -13,6 +15,19 @@ public class Jeeprotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))//if press A
+        {
+            if (Angle == 0)//set to 30 degrees when it's 0
+            {
+                Angle = 30f;
+            }
+            else//set to 0 degrees when it's 30
+            {
+                Angle = 0f;
+            }
+        }
+        float currentAngle = transform.eulerAngles.z;
+        transform.eulerAngles = new Vector3(0, 0, Mathf.MoveTowardsAngle(currentAngle, Angle, rotationSpeed * Time.deltaTime));
+        //https://docs.unity3d.com/2022.3/Documentation/ScriptReference/Mathf.MoveTowardsAngle.html
     }
 }
